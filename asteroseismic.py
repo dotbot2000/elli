a99b6919fb5d8d352dac87d60000ab4113b6fd16
Aaron Dotter <aaron.dotter@gmail.com>
2016-03-11 09:53:35 +1100

    adding more info to asteroseismic.py

--- asteroseismic.py
+++ asteroseismic.py
@@ -1,7 +1,8 @@
 from numpy import sqrt
 #reference values; Teff from IAU 2015 Resolution B3
-delta_nu_sol = 135.1
-nu_max_sol = 3090.0
+#asteroseismic values from Huber et al. 2011
+delta_nu_sol = 135.1 #sigma=0.1
+nu_max_sol = 3090.0 #sigma=30.
 Teff_sol = 5772.0 
 
 def delta_nu_func(M_Mo,Teff,L_Lo):
